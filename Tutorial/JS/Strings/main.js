6a050d01ad6cec4db660563c95e23af8c26dbbc7
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-09-23 12:11:55 +0200

    Strings

---
+++ Tutorial/JS/Strings/main.js
@@ -0,0 +1,43 @@
+// function check (password) {
+    
+//     if (password.length > 8) {
+//         for (let i=0; i<password.length; i++) {
+//             if (password[i] ==  "$") {
+//                 console.log('valid')
+//                 break;
+//             }
+//         }
+//     } else {
+//         return console.log('invalid')``
+//     }
+
+  
+// }
+
+// console.log(check("e$ren1234"));
+
+///////////////////////new exercise
+
+function drEvil (value) {
+
+    if (value > 1000000) {
+        console.log (value + ' dollars ' + '(pinky)')
+    }
+
+    else {
+        console.log('wrong')
+    }
+}
+
+drEvil (10000001);
+
+///////////////////////// new exercise
+
+function mixUp(string1, string2) {
+    return console.log(string2.concat(" ", string1))
+}
+
+
+mixUp('Hello' , 'World');
+
+//new exercise
