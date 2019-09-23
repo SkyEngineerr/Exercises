10025bd8eba075ce267bcf9af21cb7becb66a22a
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-09-23 14:02:22 +0200

    Strings

--- Tutorial/JS/Strings/main.js
+++ Tutorial/JS/Strings/main.js
@@ -1,18 +1,18 @@
-// function check (password) {
+function check (password) {
     
-//     if (password.length > 8) {
-//         for (let i=0; i<password.length; i++) {
-//             if (password[i] ==  "$") {
-//                 console.log('valid')
-//                 break;
-//             }
-//         }
-//     } else {
-//         return console.log('invalid')``
-//     }
+     if (password.length > 8) {
+         for (let i=0; i<password.length; i++) {
+             if (password[i] ==  "$") {
+                 console.log('valid')
+                 break;
+             }
+         }
+     } else {
+         return console.log('invalid')``
+     }
 
   
-// }
+ }
 
 // console.log(check("e$ren1234"));
 
@@ -41,3 +41,72 @@ function mixUp(string1, string2) {
 mixUp('Hello' , 'World');
 
 /////new exercise
+
+function fixStart(s) {
+    var c = s.charAt(0);
+    return c + s.slice(1).replace(new RegExp(c, 'g'), '*');
+  }
+
+fixStart ('babble')
+
+/////////////////////// new exercise
+
+let str1 = 'The quick brown fox jumps over the lazy dog'
+let searchTerm = 'brown'
+let replaceBy = 'blue'
+
+
+console.log(str1.replace(searchTerm, replaceBy));
+
+
+
+//////////////////////// new exercise
+
+
+function check2 (password) {
+    
+    if (password.length > 8 && password.includes('$')) {
+                console.log('valid')  
+        }
+     else {
+        return console.log('invalid')``
+    }
+}
+
+check2 ('eren$12123')
+
+////////////////////// new exercise
+
+function verbing (str) {
+    let newStr = str.slice(str.length-3)
+    if (str.length > 3 && newStr == 'ing') {
+        console.log(str.concat('ly'))
+    }
+
+    else if (str.length > 3) {
+        console.log(str.concat('ing'))
+    }
+
+    else (
+        console.log(str)
+    )
+}
+
+verbing('going')
+
+/////////////////////////////new exercise
+
+function checkStr (str, word) {
+    if (str.includes(word)) {
+        console.log(str.replace(word, 'BLACK'));
+    }
+
+    else{
+        console.log(str);
+    }
+
+}
+
+checkStr('The quick brown fox jumps over the lazy dog', 'jumps')
+
+////
