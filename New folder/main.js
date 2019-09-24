8e06ce4fb8570e26cf9be29d9b44984d59d8057f
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-09-24 10:22:01 +0200

    .

--- New folder/main.js
+++ New folder/main.js
@@ -1,13 +1,30 @@
-function prompt_box()
-{
-
-    var name = prompt("Please enter your name","Name");
-
-    if(name!=null && name!= " ")
-    {
-
-        document.write("Hello" + " " + name 
-        + " " + "Thank You!");
-
+function myFunction() {
+    var person = prompt();
+    
+    if (person == false) {
+        document.write('Please write any name');
     }
-}
+
+    else if (person != null) {
+        document.write ("Hello " + person + "! How are you today?");
+      }
+  }
+
+  myFunction();
+
+
+
+  function squared() {
+    var person = prompt("write a number");
+    
+
+    if (!isNaN(person)) {
+        document.write("result: " + person*person);
+    }
+
+    else {
+        document.write ('write a number');
+      }
+  }
+
+  squared();
