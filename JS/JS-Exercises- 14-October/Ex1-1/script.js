6e33943c5a51f8caeedff70bab47677a9c5acc64
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-10-14 11:14:15 +0200

    ex2

--- JS/JS-Exercises- 14-October/Ex1-1/script.js
+++ JS/JS-Exercises- 14-October/Ex1-1/script.js
@@ -1,22 +1,39 @@
+// function checkCity (city, cities) {
+
+//     for (let i = 0; i<cities.length; i++) {
+        
+//         if (cities[i] === city) {
+//             return `You were in ${city}`
+//         }
+//         else {return `You did not visit ${city}`}
+      
+        
+//     }
+    
+// }
+
+// console.log(checkCity("istanbul", ["Ankara", "Hamburg", "istanbul", "Mardin"]));
+
+
 function checkCity (city, cities) {
-    let x = "";
+
+    let newArr = [];
+    let matches = false;
 
     for (let i = 0; i<cities.length; i++) {
-        
-        if (cities[i] === city) {
-            x =  `You were in ${city}`
-            return x;
-            break;
-            
+        matches = false;
+        for (let x = 0; x<city.length; x++) {
+            if (cities[i] === city [x])
+            matches = true;
         }
-        else {x = `You did not visit ${city}`}
-        return x;
+        
+        if(matches) 
+            {newArr.push(cities[i])}
         
     }
     
-    
+    return newArr;
 }
 
 
-
-console.log(checkCity("istanbul", ["Ankara", "Hamburg", "istanbul", "Mardin"]));
+console.log(checkCity(["Kalana", "Ankara", "Hamburg"], ["Ankara", "Hamburg", "istanbul", "Mardin"]));
