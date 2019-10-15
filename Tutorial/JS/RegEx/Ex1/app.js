3f753289a40f7d43a87467dc64f8de86450232e3
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-10-15 10:07:36 +0200

    RegEx

---
+++ Tutorial/JS/RegEx/Ex1/app.js
@@ -0,0 +1,18 @@
+let re;
+re = /hello/;
+re = /hello/i;
+
+
+//console.log(re);
+//console.log(re.source);
+
+//exec() return result in an array or null
+// const result = re.exec('When Sacha come late today we all say hello');
+// console.log(result);
+// console.log(result[0]);
+// console.log(result[1]);
+// console.log(result.index);
+// console.log(result.input);
+
+const
+
