616052118b0256ee4f7410698d25ce30bb821af3
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-10-15 15:09:05 +0200

    RegEx

--- Tutorial/JS/RegEx/Ex2/app.js
+++ Tutorial/JS/RegEx/Ex2/app.js
@@ -9,9 +9,17 @@ re = /world$/i; // must ends with (complete word)
 re = /^world$/gi; //must include only this word
 re = /h..llo/i; // maches any One character
 re =/gre?a?y/i; //optional character (dolar isaratenin solundaki karakter icin gecerli)
+re =/gre?a?y\?$/i;
+
+//Brackets [] - Character Sets
+re = /gr[ae]y/i; //must be an a or e
+re = /[GF]ray/i; //must be a g or f
+re = /[^GF]ray/i; //match anything except g or f
+re = /[A-Z]ray/i;
+
 
 //string to match
-const str = 'grey';
+const str = 'graey';
 
 //Log Results
 const result = re.exec(str);
