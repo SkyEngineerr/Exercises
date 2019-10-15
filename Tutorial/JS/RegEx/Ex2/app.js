06954b48051e4886a1c2c5d26c227b32950c0430
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-10-15 13:57:59 +0200

    RegEx

--- Tutorial/JS/RegEx/Ex2/app.js
+++ Tutorial/JS/RegEx/Ex2/app.js
@@ -1,57 +1,28 @@
-// let re;
-// re = /hello/;
-// re = /hello/i;
+let re;
+//literal characters
+re = /hello/i;
 
+//metacharacter symbols 
+re = /^h/i; //must start with 
+re = /d$/i; // must ends with (one char)
+re = /world$/i; // must ends with (complete word)
+re = /^world$/gi; //must include only this word
+re = /h..llo/i; // maches any One character
+re =/gre?a?y/i; //optional character (dolar isaratenin solundaki karakter icin gecerli)
 
-//console.log(re);
-//console.log(re.source);
+//string to match
+const str = 'grey';
 
-// 1 exec() return result in an array or null
-//exec() 
-// const result = re.exec('When Sacha come late today we all say hello');
-// console.log(result);
-// console.log(result[0]);
-// console.log(result[1]);
-// console.log(result.index);
-// console.log(result.input);
+//Log Results
+const result = re.exec(str);
+console.log(result);
 
-//2 test() true or falase
-// const result = re.test('Hello');
-// console.log(result);
-
-// 3 match ()
-// const str = 'Hello There';
-// const result = str.match(re);
-// console.log(result);
-
-// function checkWord (test, word) {
-//     let ch = /test/gi;
-//     let result = ch.test(word);
-//     return result;
-// }
-
-
-
-// console.log(checkWord("kaleman", 'a'));
-
-//4 search () - returns index of the first match,
-//if not found it returns -1
-
-// const str = 'Hello There';
-// const result = str.search(re);
-// console.log(result);
-
-//5 replace () - Returns new String 
-//with some or all matches of a pattern
-// const str = 'Hello There';
-// const newStr = str.replace(re, 'Hi');
-// console.log(newStr);
-
-function replaceWord (str, regEx,  newStr) {
-    
-    let result = str.replace(regEx, newStr);
-    return result;
+function reTest (re, str) {
+    if (re.test(str)){
+        console.log(`${str} matches ${re.source}`)
+    } else{
+        console.log(`${str} does not match ${re.source}`)
+    }
 }
 
-
-console.log(replaceWord("Hello There", /hello/i, 'Hi'  ))
+reTest(re, str)
