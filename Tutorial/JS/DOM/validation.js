15b1c1fd3d4cd3b284f6988bef5a80bc2f169806
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-10-21 15:22:52 +0200

    DOM

---
+++ Tutorial/JS/DOM/validation.js
@@ -0,0 +1,17 @@
+//Form Blur Event Listeners
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
+
+
+//Implementing the validators
+function validateName (){
+    const name = document.getElementById('name');
+    const re = /^[a-z]{2,10}$/i; 
+    if(!re.test(name.value)) {
+        name.classList.add('is-invalid');
+    } else {
+
+    }
+}
