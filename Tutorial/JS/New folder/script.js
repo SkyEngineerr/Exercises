5843d91ade209550bad246cfdb4d61c8bd9c8a3d
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-10-28 15:40:47 +0100

    primitive data types

---
+++ Tutorial/JS/New folder/script.js
@@ -0,0 +1,13 @@
+//PRIMIIVE
+
+//number
+const age = 45;
+console.log(typeof age);
+
+//boolean
+const hasChild = false;
+console.log(typeof hasChild);
+
+//null
+const car = null;
+console.log(typeof car);
