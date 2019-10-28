623216db69a593531a4d2ed472e5a34b5661e2f6
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-10-28 12:11:23 +0100

    Variable

--- Tutorial/JS/Variables/main.js
+++ Tutorial/JS/Variables/main.js
@@ -1,28 +1,89 @@
-// var, let, const
-
-var name = 'John Doe';
+// SCOPE and variables
+// var,
+// let,
+// const. -
+// what is a variable ? it is a binding factor.
+// While other languages have static variables, javascroipt does not!!!
+//     They can be changed as we wish:
+//     var (old school)
+// const +
+// let (new kid on the blog)
+// var = local scope only in a
+// function
+// const /
+// let = in functions as well as in a blog scope(a blog in curly brakets.)
+// example:
+// declare a variable with var
+//var name = "Johe Don";
+//console.log(name);
+// declare a variable with let
+//let email = "adress@domain.suffix";
+//console.log(email);
+//re assign it
+//name = 'Safwan'
+// email = 'safwan@gmail.com'
+// console.log(name, email);
+// var G & L scope
+// function show() {
+//     var nun = 5;
+//     console.log(nun);
+// }
+// var nun = 9
+// show();
+// console.log(nun); {
+//     var num = 7
+//     console.log(num);
+// }
+// console.log(num);
+// let and const G & L Scope
+// function print() {
+//     let number = 107;
+//     console.log(number)
+// } {
+//     let number = 123;
+//     console.log(number)
+// }
+// let number = 109;
+// console.log(number);
+// print();
+//init - declaring a varibale without giving it value
+// this is allowed only using var or let
+// var somThing;
+// console.log(typeof somThing);
+// varibale Name can consist of numbers , ,latters, _ $
+//Can not start with Numbers
+// let Name1 = 'Olli';
+//recommended camleCase names
+// let firstName = 'Ali';
+// let secoundName = 'Stefn';
+// let fullName = firstName + secoundName;
+// console.log(fullName);
+//const
+const name = 'Johe'
 console.log(name);
-
-let email = 'address@domain.suffix'
-console.log(email);
-//reassign it
-name = 'Eren'
-email = 'eren@gmaiil.com'
-console.log(name, email);
-
-// car Global & Local Scope
-function show (){
-    var num = 5;
-    console.log(num);
+//can't reAssign a new value
+//name = 'steve';
+//can't init
+//const someThing;
+//exceptions to const
+//objects and Arrays can be mutated
+//object example :
+const person = {
+    name: 'Ray',
+    age: 23
 }
-
-var num = 9;
-show()  
-console.log(num);
-
-{
-    var num = 7;
-    console.log(num);
-}
-
-console.log(num);
+person.name = 'Jay';
+person.age = 25;
+console.log(person);
+//But not like below ;
+// person = {
+// name: 'Ray',
+//     age: 23
+// }
+const arr = [1, 2, 3, 4, 5]
+arr.push('added Value')
+console.log(arr);
+arr[3] = 'changed Value'
+console.log(arr);
+//but Not
+//arr = [44,55]
