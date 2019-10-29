1b3187dff0468cf44e22de6c5b44213fc33e0050
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-10-29 09:19:55 +0100

    .

--- Tutorial/JS/New folder/script.js
+++ Tutorial/JS/New folder/script.js
@@ -11,3 +11,44 @@ console.log(typeof hasChild);
 //null
 const car = null;
 console.log(typeof car);
+
+//undefined
+
+let test = undefined
+console.log(typeof test);
+
+//symbol
+const sym = Symbol();
+console.log(sym);
+
+//REFERENCE TYPES - Objects
+
+
+//Array
+const hobbies = ['coding', 'cooking'];
+console.log(typeof hobbies);
+
+//Object literal
+const address = {
+    city: 'Dubai',
+    country: 'UAE'
+}
+
+console.log(typeof address);
+
+//date
+const today = new Date()
+console.log(typeof today);
+
+let x = {
+    name:'EREN',
+    age: 30
+}
+
+let y = 1200;
+let a,b;
+a = x;
+
+console.log(a);
+x.name = 'Ahmet'
+console.log(a);
