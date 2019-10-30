b8e150683ea9869dff15d316c927eaeb12860045
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-10-30 10:12:40 +0100

    .

--- Tutorial/JS/section 6/script.js
+++ Tutorial/JS/section 6/script.js
@@ -27,3 +27,59 @@ console.log(val);
 //charAt()
 val = firstName.charAt(4)
 console.log(val);
+
+//Get last char
+val = firstName[firstName.length-1]
+
+//substring()
+val = str. substring(0,4)
+console.log(val);
+
+//slice
+val = str.slice(0,4)
+val = str.slice(-3)
+console.log(val);
+
+//split()
+val = str.split(' ')
+console.log(val);
+
+//includes()
+
+
+
+
+
+
+function leftDigit(num) {
+    let arr = num.split('');
+  let newArr = [];
+
+ for (let i = 0; i < arr.length; i++) {
+   if (!isNaN(arr[i])) {
+    return Number(num[i])
+   }
+ }
+
+}
+
+
+leftDigit("U//DertHe1nflu3nC3")
+
+
+
+
+function findCountMyName (str) {
+    let arr = str.split(' ')
+    let count = 0
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == 'Eren') {
+        count = count + 1
+      }
+    }
+  
+    return `Eren found ${count} times in the text`
+  }
+  
+  
+  findCountMyName ('Eren go to school and Eren eat lahmacun.')
