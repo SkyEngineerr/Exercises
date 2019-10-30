b98183a48d098d996170fd706890b0df268f9c7a
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-10-30 11:46:08 +0100

    .

---
+++ Tutorial/JS/Array Methods/script.js
@@ -0,0 +1,37 @@
+//Creat some arrays
+const numbers = [43, 56, 33, 23, 36, 5];
+const numbers2 = new Array(43, 56, 33, 23, 36, 5);
+const fruits = ['Apple', 'Banana', 'Orange', 'Pear']
+const mixed = [22, 'Hello', true, undefined, null, {
+    a: 1,
+    b: 2
+}, new Date()];
+let val;
+//Get array length
+val = numbers.length;
+//check if is array
+val = Array.isArray(mixed);
+//Access the array elements
+val = numbers[2]
+val = mixed[4];
+//Edit into the Array
+numbers[2] = 100;
+//Find index of vaule
+val = numbers.includes(33);
+//Mutating Arrays
+//Add on the end
+numbers.push(250)
+//Add on the front
+numbers.unshift(4)
+//Remove item from end
+numbers.pop();
+//remove item from front
+numbers.shift();
+//Splice values
+val = numbers.splice(0, 1);
+console.log(numbers)
+console.log(val)
+
+
+
+
