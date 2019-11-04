6b20e0382e3132f054e50ccf36d9b16c8b2860f4
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-11-04 14:42:53 +0100

    .

---
+++ Tutorial/JS/Conditionals/script.js
@@ -0,0 +1,32 @@
+/*
+if(something){
+    // do these instructions
+} else if(condition 2){
+    // do other instructions
+}
+else {
+    // do these something else
+}
+*/
+//const id = '100';
+// EQUAL TO     
+/*
+if (id == 100){
+    console.log(true)
+} else {
+    console.log(false)
+}
+// NOT EQUAL TO     
+if (id != 100){
+    console.log(true)
+} else {
+    console.log(false)
+}
+*/
+let id ='100';
+//TEST IF UNDEFINED 
+if(typeof id == `undefined`){
+    console.log(`id is ${id}`)
+} else {
+    console.log(`It is defined and the value is ${id}`)
+}
