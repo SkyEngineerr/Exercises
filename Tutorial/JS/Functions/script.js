6dc141d58ba30f6ae4e61b34d37f6f148a1f6103
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-11-05 14:51:34 +0100

    .

--- Tutorial/JS/Functions/script.js
+++ Tutorial/JS/Functions/script.js
@@ -13,16 +13,85 @@ function greet(firstName){
 }
 greet('Filiz');
 */
-function greet(firstName){
-  return 'HEllo ' + firstName
+
+// function greet(firstName){
+//   return 'HEllo ' + firstName
+// }
+// let val = greet('Safwan')
+// console.log(val)
+
+
+//FUNCTION EXPRESSION
+
+
+// function greet(firstName = 'EREN'){
+//   return 'HEllo ' + firstName
+// }
+// let val = greet('Safwan')
+// console.log(val)
+
+
+// const squar = function (x) {
+//   return typeof x !== "number" ? x: x*x
+// }
+
+// console.log(squar(12))
+
+//IMMEDIATELY INVOKABLE FUNCTION EXPRESSIONS (IIFES)
+
+(function(x){
+  console.log(x*x);
+})()
+
+
+function squarArr (arr) {
+  let newArr = []
+  arr.forEach(function(element) {
+    newArr.push(element*element)
+});
+
+return newArr
 }
-let val = greet('Safwan')
-//console.log(val)
-Collapse
+ 
+squarArr([1,2,3,4])
 
+const todo = {
+  newArr : [],
+  add:function(item){
+    this.newArr.push(item)
+  },
+  edit:function(id){
+    console.log(`edit todo ${id}`);
+  }
 
-
-
-
+}
   
-  
+todo.add('12');
+todo.edit(4);
+console.log(todo.newArr);
+
+
+function checkAll () {
+  for (let i =0; i<500; i++) {
+    if(i<100 ) {
+      containerCount += 1
+      weight += 10
+    }
+
+    else if(i<200 ) {
+       containerCount += 1
+        weight += 20
+    }
+    else if(i<250 0) {
+       containerCount += 1
+        weight += 50
+    }
+    else if(i<300 ) {
+       containerCount += 1
+        weight += 100
+    }
+    else if(i<300 ) {
+       containerCount += 1
+        weight += 100
+    }
+}
