88660d1ceb0c519d087fd0f178ae71914fa13f96
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-11-19 09:40:14 +0100

    dom

--- Tutorial/HTML DOM/Selectors for Multiple Elements/script.js
+++ Tutorial/HTML DOM/Selectors for Multiple Elements/script.js
@@ -1,2 +1,7 @@
-let val = document.getElementById('task-title')
+//document.getElementsByClassName
+const items = document.getElementsByClassName('collection-item')
+console.log(items);
+let val = document.querySelector('h5')
 console.log(val);
+
+
