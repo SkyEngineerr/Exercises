e6d2b60100788de8629ce1c8e121bb3642c50ad1
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-11-19 11:32:02 +0100

    .

--- Tutorial/HTML DOM/Selectors for Multiple Elements/script.js
+++ Tutorial/HTML DOM/Selectors for Multiple Elements/script.js
@@ -2,15 +2,31 @@
 //const listItems = document.querySelector('ul').getElementsByClassName('collection-item')
 
 
-const items = document.getElementsByClassName('collection-item')
-console.log(items);
-items[0].style.color= 'red'
-items[2].textContent ='Hello'
+//const items = document.getElementsByClassName('collection-item')
+//console.log(items);
+//items[0].style.color= 'red'
+//items[2].textContent ='Hello'
 
 
 //document.getElementsByTagName('li')
 
 let lis =  document.getElementsByTagName('li')
+// let colors = ['brown','green','orange','red','blue']
+// let texts = ['brown','green','orange','red','blue']
+// for (let i =0; i<lis.length; i++) {
+  
+//   lis[i].textContent = `The color is ${texts[i]}`
+//   lis[i].style.color = colors[i]
+
+// }
+
+// console.log(lis);
+
+//Convert HTML Collection into array
+
+
+lis = Array.from(lis);
+
 console.log(lis);
 
 
