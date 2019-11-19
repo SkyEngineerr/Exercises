308e377f14e933eae0a6e034dc0ad689f2cb227e
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-11-19 14:11:36 +0100

    .

--- Tutorial/HTML DOM/Selectors for Multiple Elements/script.js
+++ Tutorial/HTML DOM/Selectors for Multiple Elements/script.js
@@ -10,33 +10,51 @@
 
 //document.getElementsByTagName('li')
 
-let lis =  document.getElementsByTagName('li')
-let colors = ['brown','green','orange','red','blue']
-let texts = ['brown','green','orange','red','blue']
-for (let i =0; i<lis.length; i++) {
+// let lis =  document.getElementsByTagName('li')
+// let colors = ['brown','green','orange','red','blue']
+// let texts = ['brown','green','orange','red','blue']
+// for (let i =0; i<lis.length; i++) {
   
 //   lis[i].textContent = `The color is ${texts[i]}`
 //   lis[i].style.color = colors[i]
 
-}
+// }
 
 // console.log(lis);
 
 //Convert HTML Collection into array
 
 
-lis = Array.from(lis);
+// lis = Array.from(lis);
 
-console.log(lis);
+// console.log(lis);
 
-lis.reverse();
+// lis.reverse();
 
-lis.forEach(function(item,index){
-  item.innerHTML = `<span style='color:red;'>${index} : Hello</span> `
+// lis.forEach(function(item,index){
+//   item.innerHTML = `<span style='color:red;'>${index} : Hello</span> `
+// })
+
+//document.querySelectorAll()
+const items2 = document.querySelectorAll("li")
+console.log(items2);
+
+
+items2.forEach(function(item,index){
+  console.log(item);
+  item.style.color = 'blue'
+  item.innerHTML = `<a href="https://www.linkedin.com/in/erenyilmaz123/" target="_blank">Click It</a>`
 })
 
+const liOdd= document.querySelectorAll('li:nth-child(odd)')
+const liEven= document.querySelectorAll('li:nth-child(even)')
 
+liOdd.forEach (function(li, index) {
+  li.style.background= 'red'
+})
 
+liEven.forEach (function(li, index) {
+  li.style.background= 'yellow'
+})
 
-
-
+console.log(liOdd);
