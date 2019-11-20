e7ee5dc2a5574bb5c7643a541fa2b86ba3be0bba
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-11-20 11:14:59 +0100

    dom children

--- Tutorial/HTML DOM/Traversing DOM/script.js
+++ Tutorial/HTML DOM/Traversing DOM/script.js
@@ -1,11 +1,12 @@
 let val;
 const list = document.querySelector('ul.collection')
+
 const listItem = document.querySelector('li.collection-item:first-child');
 
 val = list;
-console.log(val);
+//console.log(val);
 val = listItem
-console.log(val);
+//console.log(val);
 
 //Get Child -Nodes-
 val = list.childNodes
@@ -13,17 +14,15 @@ let val2 = Array.from(val)
 // val = list.childNodes[0]
 // val = list.childNodes[0].nodeName
 //val = list.childNodes[0].nodeType
-console.log(val)
+//console.log(val)
 
 
   val2.forEach(function(item){
-    console.log(item);
-    if(item.innerText == 'hello'){
-      console.log('hahahah');
+  
+    if(item.innerText == 'Hello'){
+      item.style.color = 'red'
     }
   })
-
-
 
 // 1=Element
 // 2=Attribute
@@ -31,3 +30,22 @@ console.log(val)
 // 8=Comment
 // 9=Document itself
 // 10=Doctype
+
+
+val = list.children;
+val = val[0].className
+val = list.children[1].innerText
+console.log(val);
+
+list.children[0].children[0].id = 'test-link'
+console.log(list.children[0].children[0].children[0]);
+
+//First Child
+
+val = list.firstChild;
+val = list.firstElementChild;
+val = list.firstElementChild.innerText
+val = list.lastElementChild.innerText
+val = list.children.length
+console.log(val);
+
