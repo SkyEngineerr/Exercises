9c00a83821f37bd5554883c93177088679a9dfa2
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-11-20 15:32:35 +0100

    Append Dom

--- Tutorial/HTML DOM/Creating Elements/script.js
+++ Tutorial/HTML DOM/Creating Elements/script.js
@@ -1,93 +1,35 @@
-let val;
-const list = document.querySelector('ul.collection')
+//////////////////////////////////
 
-const listItem = document.querySelector('li.collection-item:first-child');
+//Create Element
+const li = document.createElement('li')
 
-console.log(listItem);
-val = list;
-//console.log(val);
-val = listItem
-//console.log(val);
-
-//Get Child -Nodes-
-val = list.childNodes
-let val2 = Array.from(val)
-// val = list.childNodes[0]
-// val = list.childNodes[0].nodeName
-//val = list.childNodes[0].nodeType
-//console.log(val)
+//Add Class
+li.className = "collection-item"
+//Add Id
+li.id = 'new-id'
+//Add attributes
+li.setAttribute('title', 'New Item')
+//Create text node and append
+li.appendChild(document.createTextNode('1111'))
 
 
-  // val2.forEach(function(item){
-  
-  //   if(item.innerText == 'Hello'){
-  //     item.style.color = 'red'
-  //   }
-  // })
+//create new link element
+const link = document.createElement('a')
 
-// 1=Element
-// 2=Attribute
-// 3=Text node
-// 8=Comment
-// 9=Document itself
-// 10=Doctype
+//add classes
+link.className = 'delete-item secondary-content'
+
+//add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>'
+
+//Append link into li
+li.appendChild(link)
 
 
-val = list.children;
-val = val[0].className
-val = list.children[1].innerText
-//console.log(val);
+//Append li as child to ul
+document.querySelector('ul.collection').appendChild(li)
 
-list.children[0].children[0].id = 'test-link'
-//console.log(list.children[0].children[0].children[0]);
+console.log(li);
 
-//First Child
+///////////////////////////////////
 
-val = list.firstChild;
-val = list.firstElementChild;
-val = list.firstElementChild.innerText
-val = list.lastElementChild.innerText
-val = list.children.length // same: list.childElementCount
-
-
-//Get parent node
-val = listItem.parentNode
-val = listItem.parentElement
-val = listItem.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement
-
-
-////
-
-// val = list.children
-// val = Array.from(val)
-// console.log(val);
-
-// val.forEach(function(item){
-//   console.log(item);
-//   console.log(item.style.color );
-//   if(item.style.color == 'red') {
-//     item.innerText = 'DONE'
-//   }
-// })
-
-// let count =0;
-// val.forEach(function(item,index){
-  
-
-// if(item.innerText == 'DONE') {
-//     count += 1
-//   }
-// })
-
-// console.log(count);
-
-
-//get next sibling
-val = listItem.nextSibling
-val = listItem.nextElementSibling
-console.log(val);
-
-//get prev sibling
-val = listItem.previousSibling
-val = listItem.previousElementSibling
-console.log(val);
