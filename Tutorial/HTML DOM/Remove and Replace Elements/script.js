88294263280445310b0d4e734a5077156ab3a13b
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-11-25 14:16:40 +0100

    textNode

--- Tutorial/HTML DOM/Remove and Replace Elements/script.js
+++ Tutorial/HTML DOM/Remove and Replace Elements/script.js
@@ -1,93 +1,36 @@
-let val;
-const list = document.querySelector('ul.collection')
+//REPLACE ELEMENT
+//Create Element
 
-const listItem = document.querySelector('li.collection-item:first-child');
+const newHeading = document.createElement("h2");;
 
-console.log(listItem);
-val = list;
-//console.log(val);
-val = listItem
-//console.log(val);
+//Add id
+newHeading.id = "task-title";
 
-//Get Child -Nodes-
-val = list.childNodes
-let val2 = Array.from(val)
-// val = list.childNodes[0]
-// val = list.childNodes[0].nodeName
-//val = list.childNodes[0].nodeType
-//console.log(val)
+//New text node
+newHeading.appendChild(document.createTextNode("Easy Busy"))
 
+//Get the old heading
+const oldHeading = document.getElementById('task-title')
 
-  // val2.forEach(function(item){
-  
-  //   if(item.innerText == 'Hello'){
-  //     item.style.color = 'red'
-  //   }
-  // })
+//Parent
+const cardAction = document.querySelector(".card-action")  //Same down
+const parentElement = oldHeading.parentElement // Same above
 
-// 1=Element
-// 2=Attribute
-// 3=Text node
-// 8=Comment
-// 9=Document itself
-// 10=Doctype
+//Replace
+cardAction.replaceChild(newHeading, oldHeading)
+
+//Replace textnode
+const li =document.querySelector('li')
 
 
-val = list.children;
-val = val[0].className
-val = list.children[1].innerText
-//console.log(val);
 
-list.children[0].children[0].id = 'test-link'
-//console.log(list.children[0].children[0].children[0]);
+///EXERCISE REPLACE TEXT INNER LI LIST///
+// const firstText = document.querySelector('li').childNodes[0]
 
-//First Child
+// const li = document.querySelector('.collection-item')
 
-val = list.firstChild;
-val = list.firstElementChild;
-val = list.firstElementChild.innerText
-val = list.lastElementChild.innerText
-val = list.children.length // same: list.childElementCount
+// let text = document.createTextNode('CHANGED ')
 
+// li.replaceChild(text, firstText)
+/////////////////////////////////////////
 
-//Get parent node
-val = listItem.parentNode
-val = listItem.parentElement
-val = listItem.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement
-
-
-////
-
-// val = list.children
-// val = Array.from(val)
-// console.log(val);
-
-// val.forEach(function(item){
-//   console.log(item);
-//   console.log(item.style.color );
-//   if(item.style.color == 'red') {
-//     item.innerText = 'DONE'
-//   }
-// })
-
-// let count =0;
-// val.forEach(function(item,index){
-  
-
-// if(item.innerText == 'DONE') {
-//     count += 1
-//   }
-// })
-
-// console.log(count);
-
-
-//get next sibling
-val = listItem.nextSibling
-val = listItem.nextElementSibling
-console.log(val);
-
-//get prev sibling
-val = listItem.previousSibling
-val = listItem.previousElementSibling
-console.log(val);
