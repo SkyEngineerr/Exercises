bcdc1608176b50460f2fb5761f001817be9c4392
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-11-26 14:42:34 +0100

    .

--- Tutorial/HTML DOM/Event Listener/script.js
+++ Tutorial/HTML DOM/Event Listener/script.js
@@ -1,58 +1,24 @@
-//REPLACE ELEMENT
-//Create Element
+//document.querySelector('.clear-tasks')
+//console.log(('clear-tasks'));
+document.querySelector('.clear-tasks').addEventListener('click', function(e){
+  console.log('Hello World');
+  e.preventDefault();
+  });
+  //document.querySelector('.clear-tasks').addEventListener('click', onClick);
+  document.querySelector('.clear-tasks').addEventListener('click', myFunc);
+  function myFunc(e){
+    //console.log('clicked!!!')
+    let val;
+    val = e;
+    //Event target element
+    val = e.target;
+    val = e.target.id
+    val = e.target.className
+    val = e.target.classList
+    e.target.className = "clear-tasks btn red"
+    
+   
+    console.log(val)
+  }
 
-const newHeading = document.createElement("h2");;
-
-//Add id
-newHeading.id = "task-title";
-
-//New text node
-newHeading.appendChild(document.createTextNode("Easy Busy"))
-
-//Get the old heading
-const oldHeading = document.getElementById('task-title')
-
-//Parent
-const cardAction = document.querySelector(".card-action")  //Same down
-const parentElement = oldHeading.parentElement // Same above
-
-//Replace
-cardAction.replaceChild(newHeading, oldHeading)
-
-//Remove Element
-// const lis = document.querySelectorAll('li');
-// const list = document.querySelectorAll("ul")
-// lis[0].remove();
-
-// const li2 = list.children[0]
-// list.removeChild(li2)
-// console.log(lis);
-
-
-//ATTRIBUTES
-val = link.getAttribute('href')
-  link.setAttribute('href', 'http://google.com')
-  val = link
-console.log(val);
-
-///EXERCISE REPLACE TEXT INNER LI LIST///
-// const firstText = document.querySelector('li').childNodes[0]
-
-// const li = document.querySelector('.collection-item')
-
-// let text = document.createTextNode('CHANGED ')
-
-// li.replaceChild(text, firstText)
-/////////////////////////////////////////
-
-
-///EXERCISE 2//////////////////////
-let texts = document.querySelectorAll('li')
-
-texts.forEach(function(item){
-   console.log(item.firstChild);
-    if(item.innerText == "DONE") {
-      item.remove()
-    }
-})
 
