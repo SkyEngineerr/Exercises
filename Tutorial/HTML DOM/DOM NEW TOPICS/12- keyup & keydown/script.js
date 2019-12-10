e1b68f1875f543f773ed5eac18bd9c747b6c02b0
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-12-10 10:12:06 +0100

    .

--- Tutorial/HTML DOM/DOM NEW TOPICS/12- keyup & keydown/script.js
+++ Tutorial/HTML DOM/DOM NEW TOPICS/12- keyup & keydown/script.js
@@ -1,5 +1,35 @@
-let fLi = document.querySelector('.chapter'); //selecting by tagName the first LI
-let x, y, z;
-x = fLi.closest('.book')
-y = fLi.closest('.contents')
-console.log(x);
+let inpt = document.querySelector("#kinput")
+let txtarea = document.querySelector("textarea")
+
+
+inpt.addEventListener('keydown', listener)
+inpt.addEventListener('keyup', listener)
+
+
+function listener (e) {
+    if(e.type == 'click') {
+        inpt.preventDefault()
+    }
+    if (e.type == 'keydown') {
+        txtarea.innerHTML = e.key
+        console.log(e.target);
+        
+    }
+    else if (e.type == 'keyup'){
+        txtarea.innerHTML = e.key
+    }
+}
+
+inpt.onkeydown = inpt.onkeyup = kinput.onkeypress = handle;
+let lastTime = Date.now();
+
+function handle (e) {
+    console.log(form.elements);
+}
+
+
+
+
+
+console.log(inpt);
+console.log(txtarea);
