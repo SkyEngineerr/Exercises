9bbad1a351e5a959e39eef009f484f19a69dfe86
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2019-12-16 11:47:23 +0100

    .

--- Tutorial/JS/4-Task List/Step-1/app.js
+++ Tutorial/JS/4-Task List/Step-1/app.js
@@ -1,7 +1,30 @@
 //Define UI Variables
 const form = document.querySelector('#task-form')
-const taskList = document.querySelector('')
+const taskList = document.querySelector('.collection')
 const clearBtn = document.querySelector('.clear-tasks')
 const filter = document.querySelector('#filter')
-const taskInput = document.querySelector('#')
+const taskInput = document.querySelector('#task')
 
+console.log(form, clearBtn, filter, taskInput);
+console.log(taskList);
+
+//Load all event listeners
+loadEventListeners();
+
+// Defining the function that Load all event listeners
+function loadEventListeners() {
+    //Add Task Event
+    form.addEventListener('submit', addTask)
+};
+
+//Add Task Event Handler
+function addTask(e){
+    e.preventDefault();
+    
+    let li = document.createElement('li')
+    li.appendChild(document.createTextNode('Eren'))
+    let a = document.createElement('a')
+    li.appendChild(a)
+
+    taskList.appendChild(li)
+}
---
+++ Tutorial/JS/4-Task List/Step-1/td.js
@@ -0,0 +1,40 @@
+document.querySelector('form').addEventListener('submit', function(e){
+    const task = document.getElementById('task').value;
+    let tasks;
+    if(localStorage.getItem('tasks') === null) {
+        tasks = []
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.push(task)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    e.preventDefault()
+    createLi()
+})
+
+function createLi(){
+    let ul = document.querySelector('.collection').children
+    //console.log(Array.from(ul.children))
+    if(ul.length>0){
+    Array.from(ul).forEach(function(item){
+        item.remove()
+    })
+}
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+if(tasks != null){
+for(j=0; j<tasks.length; j++){
+    console.log(tasks[j])
+let li = document.createElement('li');
+let a = document.createElement('a')
+a.className='delete-item secondary-content'
+let i = document.createElement('i')
+i.className = 'fa fa-remove'
+li.appendChild(a)
+a.appendChild(i)
+a.addEventListener('click', removing)
+li.className = 'collection-item'
+li.appendChild(document.createTextNode(tasks[j]))
+document.querySelector('ul.collection').appendChild(li)
+}
+}}
